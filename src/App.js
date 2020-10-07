0dbd9e4e3537967282784e3aae42dd61e2c4318f
Meghan Hein <meghan.hein@gmail.com>
2020-10-07 13:39:20 -0700

    format header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from './components/Header'
+// import Toolbar from './components/Toolbar'
+// import Canvas from './components/Canvas'
+// import Footer from './components/Footer'
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      {/* <Toolbar/>
+      <Canvas/>
+      <Footer/> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import './Header.scss'
 
+export default function Header() {
+  return (
+    <header>
+      <h3>Konva Whiteboard App</h3>
+    </header>
+  )
+}
