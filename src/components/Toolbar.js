f001abb37995300fc7cad1d2d110c00566414255
Meghan Hein <meghan.hein@gmail.com>
2020-10-09 13:39:35 -0600

    update package with needed libraries

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Konva from 'konva-react';
 import './Toolbar.scss';
 
 export default function Toolbar() {
   return (
     <div id="toolbar">
-      
+
     </div>
   )
 }
