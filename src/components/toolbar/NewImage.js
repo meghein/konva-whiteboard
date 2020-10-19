93e5a834f0ae17d43b565bc9f1b2bc52e1f1b41f
Meghan Hein <meghan.hein@gmail.com>
2020-10-19 15:10:21 -0700

    add image uploader

--- src/components/toolbar/NewImage.js
+++ src/components/toolbar/NewImage.js
@@ -1,15 +1,33 @@
-import React from 'react';
-// import { Image, Transformer } from "react-konva";
-// import useImage from "use-image";
+import React, { createRef, useState } from 'react';
 
-export default function Image() {
-  // const shapeRef = React.useRef();
-  // const trRef = React.useRef();
-  // const [image] = useImage(imageUrl);
+export default function NewImage() {  
+  const [image, setImage] = useState()
+  const fileUploadEl = createRef();
+
+  const addImage = () => {
+    fileUploadEl.current.click();
+  };
+
+  const fileChange = event => {
+    const file = event.target.files[0]
+    const reader = new FileReader();
+    reader.readAsDataURL(file)
+    reader.addEventListener("load", () => {
+      console.log(reader.result)
+      setImage(reader.result)
+    });
+  };
 
   return (
-    <div>
-      
-    </div>
-  )
-};
+    <>
+      <button onClick={addImage}>Add Image</button>
+      <input
+        type="file"
+        ref={fileUploadEl}
+        onChange={fileChange}
+        style={{display: "none"}}
+      />
+      <img src={image}/>
+    </>
+  );
+}
--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NewImage from './NewImage'
 import './Toolbar.scss';
 
 export default function Toolbar() {
   return (
     <div id="toolbar">
-
+      <NewImage/>
     </div>
   )
 }
