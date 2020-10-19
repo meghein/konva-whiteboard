8a096537286416a3917cf49a88bb1011339a2dc5
Meghan Hein <meghan.hein@gmail.com>
2020-10-19 14:01:00 -0700

    add new image file

---
+++ src/components/toolbar/NewImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Image, Transformer } from "react-konva";
+// import useImage from "use-image";
+
+export default function Image() {
+  // const shapeRef = React.useRef();
+  // const trRef = React.useRef();
+  // const [image] = useImage(imageUrl);
+
+  return (
+    <div>
+      
+    </div>
+  )
+};
